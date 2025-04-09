c03cb3ed45e7a32623a2505bee76d3f791ea3572
Kazi Tanzil <kazitanjilkazitanjil@gmail.com>
2025-04-10 02:15:45 +0600

    Add files via upload

---
+++ R_basic_practice.R
@@ -0,0 +1,66 @@
+#Add_sub_multi
+print(5 + 5)
+print(30 - 27)
+print(3 * 2 - 5)
+cat("\n")
+
+
+#Exponential
+3^4
+3**4
+cat("\n")
+
+#division
+99/7
+11%%5
+22%/%3
+cat("\n")
+
+#Variable
+a<-29
+b<-13
+c<-19
+d<-a+b-c
+print(d)
+cat("\n")
+
+#buildInFunctions_Math
+y<-c(23,16,18,13,11,9)
+min(y)
+max(y)
+sum(y)
+mean(y)
+mode(y)
+range(y)
+sd(y)
+print(y)
+cat("\n")
+
+#buildInFunctions
+cat("Value of 'pi' is =", pi, "\n")
+cat("sqrt of 25 =", sqrt(25), "\n")
+
+a1<-(-11)
+abs(a1)
+cat("abs a1= ",abs(a1))
+
+b1=89.6
+ceiling(b1)
+floor(b1)
+
+x<-"My name is Tanzil"
+tolower(x)
+toupper(x)
+strsplit(x, split="")
+
+str <- "We are the so-called \"Vikings\", from the north."
+str
+cat(str)
+
+str1 <- "I am a student of AIUB."
+nchar(str1)
+str2<-"I am in CSE department."
+paste(str1,str2)
+
+
+
